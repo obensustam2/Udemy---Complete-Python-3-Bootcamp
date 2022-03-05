fbdde06d7c140117dcee476ba5d5e74cbaf98b49
obensustam2 <obensustam2@gmail.com>
2022-03-05 23:11:00 +0300

    course is finished

---
+++ 20 - Advanced Python Objects and Data Structures/01 - Advanced Numbers.py
@@ -0,0 +1,9 @@
+import math
+
+hex_num = hex(13)
+print(hex_num)
+bin_num = bin(13)
+print(bin_num)
+
+two_decimal = round(math.pi, 2)
+print(two_decimal)
---
+++ 20 - Advanced Python Objects and Data Structures/02 - Advanced Strings.py
@@ -0,0 +1,7 @@
+s = 'hello world'
+count_o = s.count('w')
+find_o = s.find('w')
+print(count_o)
+print(find_o)
+splited = s.split('w')
+print(splited)
